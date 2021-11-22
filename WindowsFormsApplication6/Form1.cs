ab34e22df2b6a1e84582f41178fb6c127a7e08d7
yigit5334 <91451998+yigit5334@users.noreply.github.com>
2021-11-22 12:10:02 +0300

    Add files via upload

---
+++ WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                int say = 0;
+                do
+                {
+                    richTextBox1.Text = richTextBox1.Text + say.ToString();
+                    say++;
+                } while (say <= 30);
+            } 
+        }
+    }
+}
