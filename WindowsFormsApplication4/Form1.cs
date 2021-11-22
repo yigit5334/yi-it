e9ef3b41d457946e15e54fd4031109cec2040828
yigit5334 <91451998+yigit5334@users.noreply.github.com>
2021-11-22 12:08:55 +0300

    Add files via upload

---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int say = 1;
+            do
+            {
+                listBox1.Items.Add(say);
+                say++;
+            } while (say <= 10);  
+            
+                
+        }
+                
+                
+    }
+}
