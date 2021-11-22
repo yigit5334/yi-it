34cd8adf939bc38535a401a6f3d0bc96a57dd95d
yigit5334 <91451998+yigit5334@users.noreply.github.com>
2021-11-22 14:14:32 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int topla = 0;
+            for (int say = 0; say < 100;say=say+3)
+            {
+                topla=topla+say;
+                if (topla >=200)
+	{
+                    MessageBox.Show("limit aşıldı");
+                    break;
+	}
+            }
+        }
+    }
+}
