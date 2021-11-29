66d201ef73ab86d7fe378d6bd01d1f0d5b4a1bc5
yigit5334 <91451998+yigit5334@users.noreply.github.com>
2021-11-29 15:39:16 +0300

    Add files via upload

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -12,31 +12,38 @@ namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
     {
+        class Daire
+        {
+
+            public float Alan(float ycap)
+            {
+                return Convert.ToSingle(Math.PI * ycap * ycap);
+            }
+
+            public float Cevre(float ycap)
+            {
+                return Convert.ToSingle(2 * Math.PI * ycap);
+            }
+        }
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Random rnd = new Random();
-
-            for (int i = 0; i < 20; i++)
-            {
-                listBox1.Items.Add(rnd.Next(1000)); 
-            }
-            int say = 0, toplam = 0;
-            string deger;
-
-            while (say<listBox1.Items.Count)
-            {
-                deger = listBox1.Items[say].ToString();
-                toplam = toplam + Convert.ToInt16(deger);
-                say++;
-            }
-            MessageBox.Show(toplam.ToString());
-        }
-     
+        private void Form1_Load(object sender, EventArgs e) 
         
-    }
+            static void Main(string[] args)
+            {
+float yaricap;
+Daire daire = new Daire();
+ 
+Console.Write("Çemberin Yarı Çapını Giriniz :");
+yaricap = Convert.ToUInt32(Console.ReadLine());
+Console.WriteLine("Çemberin Alanı :{3:F2}",daire.Alan(yaricap));
+Console.WriteLine("Çemberin Çevresi :{5:F2}", daire.Cevre(yaricap));
+ 
+Console.ReadLine();
 }
+        }
+    }
+
